21c10df509a8c7b99bc4528a2cf27391ace4d935
Houston-Miller <houston.miller@eltoro.com>
2026-03-19 14:13:13 -0400

    web api tutorial

---
+++ WebApiLab.API/Controllers/PeopleController.cs
@@ -0,0 +1,45 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using WebApiLab.API.Models;
+
+namespace WebApiLab.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class PeopleController : ControllerBase
+    {
+        private List<Person> People {get;} = new List<Person>();
+        public PeopleController()
+        {
+            string jsonFile = System.IO.File.ReadAllText("./Resources/64KB.json");
+            var peopleData = JsonSerializer.Deserialize<List<Person>>(
+                jsonFile, 
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            if (peopleData != null)
+            {
+                People = peopleData;
+            }
+        }
+
+        // GET: api/people
+        [HttpGet]
+        public ActionResult<List<Person>> GetPeople()
+        {
+            return Ok(People);
+        }
+
+        // GET: api/people/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Person> GetPerson(string id)
+        {
+            var person = People.FirstOrDefault(p => p.Id == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return Ok(person);
+        }
+
+    }
+}
---
+++ WebApiLab.API/Program.cs
@@ -0,0 +1,51 @@
+using System.Reflection.Metadata;
+using Scalar.AspNetCore;
+using System.Text.Json;
+using WebApiLab.API.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi("v1", options =>
+    {
+        options.AddDocumentTransformer((document, context, ct) =>
+        {
+            document.Info.Title = "WebApiLab API";
+            document.Info.Version = context.DocumentName;
+            document.Info.Description = "WebLab API using OpenApi";
+            return Task.CompletedTask;
+        });
+    }
+);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+string jsonFile = File.ReadAllText("./Resources/64KB.json");
+var jsonData = JsonSerializer.Deserialize<List<Person>>(
+    jsonFile, 
+    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+app.MapGet("/people", () => jsonData)
+    .WithName("GetPeople")
+    .WithOpenApi()
+    .Produces<List<Person>>(StatusCodes.Status200OK);
+
+app.MapControllers();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi("/openapi/{documentName}.json");
+    app.MapScalarApiReference(Options =>
+    {
+        Options.Title = "WebApiLab API";
+        Options.WithOpenApiRoutePattern("/openapi/{documentName}.json");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
